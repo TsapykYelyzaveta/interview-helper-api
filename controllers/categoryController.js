7ac5ebd7b3b150641fc86a11eeef77d383687e31
Tsapyk Yelyzaveta <lizatsapik@gmail.com>
2023-03-07 14:55:51 +0200

    Images

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,125 +1,143 @@
-const Category = require('../models/categoryModel');
-const {ObjectId} = require("mongodb");
-const {sendError, sendResult, getCategoryById, getAllCategories} = require('./baseController');
+const Category = require("../models/categoryModel");
+const { ObjectId } = require("mongodb");
+const {
+  sendError,
+  sendResult,
+  getCategoryById,
+  getAllCategories,
+} = require("./baseController");
 
 module.exports = {
-    addCategory: async (req, res) => {
-        console.log("addCategory");
-        try {
-            const category = new Category(req.body);
-            await category.save();
-            sendResult(res, 'Success',
-                {
-                    "id": category._id,
-                    "title": category.title,
-                    "imageId": category.imageId,
-                    "description": category.description,
-                });
-        } catch (error) {
-            sendError(res, 400, 'Bad request')
-        }
-    },
-    editCategory: async (req, res) => {
-        console.log("editCategory");
-        try {
-            let newCategory = new Category(req.body);
-            newCategory._id = new ObjectId(req.body.id);
-            console.log('category', newCategory);
-            let category = await getCategoryById(req.body.id);
-            if (category) {
-                const oldCategory = {title: category.title, description: category.description};
-                category.title = newCategory.title ?? category.title;
-                category.description = newCategory.description ?? category.description;
-                console.log('result', category);
-                await category.save();
-                sendResult(res, 'Success', {
-                    "id": category._id,
-                    "oldTitle": oldCategory.title,
-                    "oldDescription": oldCategory.description,
-                    "title": category.title,
-                    "description": category.description,
-                });
-            } else {
-                sendError(res, 400, 'Category is missing')
-            }
-        } catch (error) {
-            sendError(res, 400, 'Bad request')
-        }
-    },
-    replaceCategory: async (req, res) => {
-        console.log("replaceCategory");
-        try {
-            let newCategory = new Category(req.body);
-            newCategory._id = new ObjectId(req.body.id);
-            if(!newCategory.title){
-                console.log('Error');
-                sendError(res, 400, 'Title is required');
-                return;
-            }
-            await Category.replaceOne({_id: new ObjectId(req.body.id)}, newCategory);
-            sendResult(res, 'Success', {
-                "id": newCategory._id,
-                "title": newCategory.title,
-                "imageId": newCategory.imageId,
-                "description": newCategory.description
-            });
-        } catch (error) {
-            sendError(res, 400, 'Bad request');
-        }
-    },
-    getCategories: async (req, res) => {
-        console.log("getCategories");
-        try {
-            const categories = await getAllCategories();
-            console.log(categories);
-            sendResult(res, 'Success', categories.map((category) => {
-                return {
-                    "id": category._id,
-                    "title": category.title,
-                    "imageId": category.imageId,
-                    "description": category.description,
-                }
-                }));
-        } catch (error) {
-            sendError(res, 400, 'Bad request')
-        }
-    },
-    getCategory: async (req, res) => {
-        console.log("getCategory");
-        try {
-            const category = await getCategoryById(req.params.id);
-            if (category) {
-                sendResult(res, 'Success', {
-                    "id": category._id,
-                    "title": category.title,
-                    "imageId": category.imageId,
-                    "description": category.description,
-                });
-            } else {
-                sendError(res, 400, 'Category is missing')
-            }
-        } catch (error) {
-            sendError(res, 400, 'Bad request')
-        }
-    },
-    deleteCategory: async (req, res) => {
-        console.log("deleteCategory");
-        try {
-            const category = await getCategoryById(req.params.id);
-            if (category) {
-                await category.remove();
-                /////Delete topics and questions/////
-                sendResult(res, 'Success', {
-                    "id": category._id,
-                    "title": category.title,
-                    "imageId": category.imageId,
-                    "description": category.description,
-                });
-            } else {
-                sendError(res, 400, 'Category is missing')
-            }
-        } catch (error) {
-            sendError(res, 400, 'Bad request')
-        }
-    },
-}
+  addCategory: async (req, res) => {
+    console.log("addCategory");
+    try {
+      const category = new Category(req.body);
+      await category.save();
+      sendResult(res, "Success", {
+        id: category._id,
+        title: category.title,
+        imageId: category.imageId,
+        description: category.description,
+      });
+    } catch (error) {
+      sendError(res, 400, "Bad request");
+    }
+  },
+  editCategory: async (req, res) => {
+    console.log("editCategory");
+    try {
+      let newCategory = new Category(req.body);
+      newCategory._id = new ObjectId(req.body.id);
+      console.log("category", newCategory);
+      let category = await getCategoryById(req.body.id);
+      if (category) {
+        const oldCategory = {
+          title: category.title,
+          description: category.description,
+          imageId: category.imageId,
+        };
+        category.imageId = newCategory.imageId ?? category.imageId;
+        category.title = newCategory.title ?? category.title;
+        category.description = newCategory.description ?? category.description;
+        console.log("result", category);
+        await category.save();
+        sendResult(res, "Success", {
+          id: category._id,
+          oldTitle: oldCategory.title,
+          oldDescription: oldCategory.description,
+          oldImageId: oldCategory.imageId,
+          title: category.title,
+          imageId: category.imageId,
+          description: category.description,
+        });
+      } else {
+        sendError(res, 400, "Category is missing");
+      }
+    } catch (error) {
+      sendError(res, 400, error);
+    }
+  },
+  replaceCategory: async (req, res) => {
+    console.log("replaceCategory");
+    try {
+      let newCategory = new Category(req.body);
+      newCategory._id = new ObjectId(req.body.id);
+      if (!newCategory.title) {
+        console.log("Error");
+        sendError(res, 400, "Title is required");
+        return;
+      }
+      await Category.replaceOne(
+        { _id: new ObjectId(req.body.id) },
+        newCategory
+      );
+      sendResult(res, "Success", {
+        id: newCategory._id,
+        title: newCategory.title,
+        imageId: newCategory.imageId,
+        description: newCategory.description,
+      });
+    } catch (error) {
+      sendError(res, 400, "Bad request");
+    }
+  },
+  getCategories: async (req, res) => {
+    console.log("getCategories");
+    try {
+      const categories = await getAllCategories();
+      console.log(categories);
+      sendResult(
+        res,
+        "Success",
+        categories.map((category) => {
+          return {
+            id: category._id,
+            title: category.title,
+            imageId: category.imageId,
+            description: category.description,
+          };
+        })
+      );
+    } catch (error) {
+      sendError(res, 400, "Bad request");
+    }
+  },
+  getCategory: async (req, res) => {
+    console.log("getCategory");
+    try {
+      const category = await getCategoryById(req.params.id);
+      if (category) {
+        sendResult(res, "Success", {
+          id: category._id,
+          title: category.title,
+          imageId: category.imageId,
+          description: category.description,
+        });
+      } else {
+        sendError(res, 400, "Category is missing");
+      }
+    } catch (error) {
+      sendError(res, 400, "Bad request");
+    }
+  },
+  deleteCategory: async (req, res) => {
+    console.log("deleteCategory");
+    try {
+      const category = await getCategoryById(req.params.id);
+      if (category) {
+        await category.remove();
+        /////Delete topics and questions/////
+        sendResult(res, "Success", {
+          id: category._id,
+          title: category.title,
+          imageId: category.imageId,
+          description: category.description,
+        });
+      } else {
+        sendError(res, 400, "Category is missing");
+      }
+    } catch (error) {
+      sendError(res, 400, "Bad request");
+    }
+  },
+};
--- models/categoryModel.js
+++ models/categoryModel.js
@@ -1,21 +1,20 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const Topic = require('../models/topicModel');
+const Topic = require("../models/topicModel");
 
 const CategorySchema = new Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    imageId: {
-        type: String,
-        required: true
-    },
-    description: {
-        type: String,
-        required: false
-    },
-
+  title: {
+    type: String,
+    required: true,
+  },
+  imageId: {
+    type: String,
+    required: false,
+  },
+  description: {
+    type: String,
+    required: false,
+  },
 });
 
 /*CategorySchema.post('save', (doc) => {
@@ -23,18 +22,17 @@ const CategorySchema = new Schema({
     console.log('doc', doc);
 });*/
 
-CategorySchema.post('remove', async (doc) => {
-    console.log('CategorySchema post remove');
-    try {
-        const topics = await Topic.find({'categoryId': doc._id});
-        if(topics.length){
-            topics.forEach(async (topic)=> await topic.remove());
-        }
-    }catch (e) {
-        console.log('Topics are absent');
+CategorySchema.post("remove", async (doc) => {
+  console.log("CategorySchema post remove");
+  try {
+    const topics = await Topic.find({ categoryId: doc._id });
+    if (topics.length) {
+      topics.forEach(async (topic) => await topic.remove());
     }
+  } catch (e) {
+    console.log("Topics are absent");
+  }
 });
 
-
-const Category = mongoose.model('categories', CategorySchema);
+const Category = mongoose.model("categories", CategorySchema);
 module.exports = Category;
